2bb5e315ca7fa253b83736fe2845abb5a12bc80a
Ly Vinh Truong <truonglv@evolableasia.vn>
2016-02-01 16:35:49 +0700

    Edit style near footer zone

--- public_html/wp-content/themes/eva_service_2016/part-contact.php
+++ public_html/wp-content/themes/eva_service_2016/part-contact.php
@@ -2,6 +2,7 @@
 <div class="container-fluid bg-dark-grey white text-center">
     <div class="container">
         <div class="row-gap-big"></div>
+        <div class="row-gap-big"></div>
         <div class="row">
             <div class="col-xs-12 no-padding-lr text-center">
                 <div class="caption full-width center-block contact-info">
@@ -9,14 +10,13 @@
                 </div>
             </div>
         </div>
-        <div class="row-gap-medium"></div>
+        <div class="row-gap-big"></div>
         <div class="row">
             <div class="col-md-12">
                 <a href="#" class="rounded-btn right-arrow yellow medium-btn">お問い合わせフォームへ</a>
             </div>
         </div>
-        <div class="row-gap-medium"></div>
-
+        <div class="row-gap-big"></div>
         <div class="row-gap-big"></div>
     </div>
 </div>
@@ -39,7 +39,7 @@
                 <div class="container contact-width">
                     <div class="row">
                         <div class="col-xs-12 col-md-6 center-block phone">
-                            <div class="detail1-text">お電話からのご相談はこちら</div>                                
+                            <div class="detail1-text">お電話からのご相談はこちら</div>
                             <div class="phone-text">(+84)8-3911-1489</div>
                             <div class="detail-text">※日本語も対応可能です。</div>
                             <span class="detail-line"></span>
