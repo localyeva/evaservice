f8add0244008814da2f8cd942e053b4892241dcc
Ly Vinh Truong <truonglv@evolableasia.vn>
2016-02-23 16:46:52 +0700

    Modify more for contact and company page

--- public_html/wp-content/themes/eva_service_2016/lib/includes/MyTheme_Customize.php
+++ public_html/wp-content/themes/eva_service_2016/lib/includes/MyTheme_Customize.php
@@ -43,20 +43,87 @@ function theme_customize_register($wp_customize) {
         'mime_type' => 'video',
     )));
 
-    $wp_customize->add_setting('part_work_environment_intro_text', array(
+    $wp_customize->add_setting('part_work_environment_intro_text_en', array(
+        'default' => '',
+    ));
+    $wp_customize->add_setting('part_work_environment_intro_text_ja', array(
+        'default' => '',
+    ));
+    $wp_customize->add_setting('part_work_environment_intro_text_vi', array(
         'default' => '',
     ));
 
-    $wp_customize->add_control('part_work_environment_intro_text', array(
+    $wp_customize->add_control('part_work_environment_intro_text_en', array(
         'label'   => __('Text in front of video here'),
         'section' => 'home',
-        'settings' => 'part_work_environment_intro_text',
+        'settings' => 'part_work_environment_intro_text_en',
+        'type'    => 'textarea',
+        'priority' => 1,
+    ));
+
+    $wp_customize->add_control('part_work_environment_intro_text_ja', array(
+        'label'   => __('テキストがビデオの前に表示され'),
+        'section' => 'home',
+        'settings' => 'part_work_environment_intro_text_ja',
+        'type'    => 'textarea',
+        'priority' => 1,
+    ));
+
+    $wp_customize->add_control('part_work_environment_intro_text_vi', array(
+        'label'   => __('Văn bản sẽ được hiển thị ở phía trước của video'),
+        'section' => 'home',
+        'settings' => 'part_work_environment_intro_text_vi',
+        'type'    => 'textarea',
+        'priority' => 1,
+    ));
+
+    $wp_customize->add_section('contact', array(
+        'title' => __('CONTACT'),
+        'priority' => 21,
+    ));
+
+    $wp_customize->add_setting('part_contact_term_text_en', array(
+        'default' => '',
+    ));
+    $wp_customize->add_setting('part_contact_term_text_ja', array(
+        'default' => '',
+    ));
+    $wp_customize->add_setting('part_contact_term_text_vi', array(
+        'default' => '',
+    ));
+
+    // Text_Editor_Custom_Control
+
+    $wp_customize->add_control('part_contact_term_text_en', array(
+        'label'   => __('Contact term'),
+        'section' => 'contact',
+        'settings' => 'part_contact_term_text_en',
+        'type'    => 'textarea',
+        'priority' => 1,
+    ));
+    $wp_customize->add_control('part_contact_term_text_ja', array(
+        'label'   => __('接触項'),
+        'section' => 'contact',
+        'settings' => 'part_contact_term_text_ja',
+        'type'    => 'textarea',
+        'priority' => 1,
+    ));
+    $wp_customize->add_control('part_contact_term_text_vi', array(
+        'label'   => __('Điều khoản liên hệ'),
+        'section' => 'contact',
+        'settings' => 'part_contact_term_text_vi',
         'type'    => 'textarea',
         'priority' => 1,
     ));
 }
 
+function theme_customize_enqueue() {
+    wp_enqueue_script( 'customizer', get_template_directory_uri() . '/js/customizer.js' );
+    // wp_enqueue_script( 'text_editor_custom_control', includes_url('js/tinymce/tinymce.min.js'), array('jquery'));
+}
+
 add_action('customize_register', 'theme_customize_register');
+add_action( 'wp_enqueue_scripts', 'theme_customize_enqueue' );
 
 function get_part_work_environment_movie_cover() {
     return esc_url(get_theme_mod('part_work_environment_movie_cover'));
@@ -73,6 +140,31 @@ function get_part_work_environment_movie_webm() {
 }
 
 function get_part_work_environment_intro_text() {
-    $text = get_theme_mod('part_work_environment_intro_text');
+    $text = '';
+    switch(get_bloginfo('language')) {
+        case 'ja':
+            $text =  get_theme_mod('part_work_environment_intro_text_ja');
+            break;
+        case 'vi':
+            $text =  get_theme_mod('part_work_environment_intro_text_vi');
+            break;
+        default:
+            $text =  get_theme_mod('part_work_environment_intro_text_en');
+    }
+    return $text;
+}
+
+function get_part_contact_term_text() {
+    $text = '';
+    switch(get_bloginfo('language')) {
+        case 'ja':
+            $text =  get_theme_mod('part_contact_term_text_ja');
+            break;
+        case 'vi':
+            $text =  get_theme_mod('part_contact_term_text_vi');
+            break;
+        default:
+            $text =  get_theme_mod('part_contact_term_text_en');
+    }
     return $text;
 }
--- public_html/wp-content/themes/eva_service_2016/lib/includes/languages/vi.php
+++ public_html/wp-content/themes/eva_service_2016/lib/includes/languages/vi.php
@@ -10,13 +10,34 @@
 	$lang['Learn more'] = "Xem thêm";
 	$lang['View all'] = "Xem tất cả";
 
-	$lang['Company Profile'] = "Hồ sơ công ty";
+	$lang['Company profile'] = "Hồ sơ công ty";
 	$lang['Affiliated company'] = "Công ty liên kết";
 	$lang['Office'] = "Văn phòng";
+    $lang['Company name'] = "Tên công ty";
+    $lang['Location'] = "Địa chỉ";
+    $lang['Phone number'] = "Điện thoại";
+    $lang['FAX'] = "FAX";
+    $lang['Officer'] = "Người đại diện";
+    $lang['Establishment'] = "Ngày thành lập";
+    $lang['Capital'] = "Vốn điều lệ";
+    $lang['License'] = "Giấy phép kinh doanh";
+    $lang['Business banks'] = "Ngân hàng giao dịch";
 
 	$lang['EVOLABLE ASIA Solution & Business Consultancy'] = "EVOLABLE ASIA Solution & Business Consultancy";
+	$lang['EVOLABLE ASIA Solution & Business Consultancy Company Limited'] = "EVOLABLE ASIA Solution & Business Consultancy Company Limited";
     $lang['m_site_description'] = "EVOLABLE ASIA Solution & Business Consultancyは、東南アジア地域で日系最大規模のラボ型開発企業であるEVOLABLE ASIAの100%子会社として設立。これまで培ってきたデジタルマーケティングに関するノウハウをコアバリューとしSolutionをカタチに実現します。";
     $lang['m_site_keywords'] = "ベトナム, web制作,動画制作,SEO対策,マーケティング, エボラブルアジア, Evolable Asia";
 
     $lang['Projects'] = 'Dự án';
     $lang['See more projects'] = 'Xem thêm các dự án';
+
+    $lang['Please fill in the following and contact us'] = 'Xin vui lòng điền vào dưới đây và liên hệ với chúng tôi.';
+    $lang['Your company name'] = 'Tên công ty của bạn';
+    $lang['Division name'] = 'Bộ phận để liên hệ';
+    $lang['Your name'] = 'Tên của bạn';
+    $lang['E-mail'] = 'E-mail';
+    $lang['Tel'] = 'Tel';
+    $lang['Contact item'] = 'Mục liên hệ';
+    $lang['Content of inquiry'] = 'Nội dung';
+    $lang['(Terms of personal information policy)'] = '(Điều khoản của chính sách thông tin cá nhân)';
+    $lang['Agree and send'] = '同意して送信する';
